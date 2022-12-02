297f9efa6eb5a267d0b00914fea616273e4941c6
kmivehchi-xRoird <kammivehchi@gmail.com>
2022-12-01 16:01:22 -0800

    added home page with responsive

--- client/src/components/styles/SingleCard.styles.js
+++ client/src/components/styles/SingleCard.styles.js
@@ -2,27 +2,44 @@ import styled from 'styled-components'
 
 
 export const Card = styled.div`
-background-color:${({ theme }) => theme.colors.primary || 'white'};
+background-color:${({ theme }) => theme.colors.cardBackground || 'white'};
 padding:1.5em;
 margin:1.5em;
-min-width:90%;
-min-height:90%;
+// min-width:90%;
+min-height:60vh;
 position:relative;
 border-radius:${({ theme }) => theme.borders.inner};
-@media (min-width: 768px) {
-min-width:50vw; 
+form:first-child{
+  display:flex;
+  justify-content:space-between;
+  div{
+    max-width:50%;
   }
+  >button{
+    color:white;
+    background-color:${({ theme }) => theme.colors.accent || 'white'};
+    border-radius:${({ theme }) => theme.borders.button};
+    padding:${({ p }) => p || ".3rem .75rem"};
+    height:fit-content;
+    
+  }
+}
+@media (min-width: 768px) {
+  max-width:50vw; 
+  
+}
 `
 
 
 export const UrlCard = styled.div`
 
 p{
-
+  
 }
 `
 export const ListContainer = styled.div`
 
 max-height:70vh;
-overflow-x:scroll;
+
+overflow-x:auto;
 `
