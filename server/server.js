c87d667ce8a49fd52da55a893e257794b9bfc271
kam-mivehchi <kammivehchi@gmail.com>
2022-12-22 02:03:07 -0500

    remove deployment env

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 
 const cors = require('cors');
 const corsOptions = {
-   origin: "https://dashboard.heroku.com/apps/collecturl" || 'http://localhost:3000',
+   origin: 'http://localhost:3000',
    credentials: true,            //access-control-allow-credentials:true
    optionSuccessStatus: 200
 }
