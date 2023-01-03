6ad41482ed7baf491e40b4c233c4b733d3a9dd1b
kam-mivehchi <kammivehchi@gmail.com>
2023-01-03 18:53:18 -0500

    fix cors

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 
 const cors = require('cors');
 const corsOptions = {
-   origin: process.env.DEPLOYMENT_URL || 'http://localhost:3000',
+   origin: process.env.DEPLOYMENT_URL,
 
    credentials: true,            //access-control-allow-credentials:true
    optionSuccessStatus: 200
